03bbbd6a35a45d09c2683954f38c70e1882d71c9
Bonface Mudiri <mudirib@gmail.com>
2017-07-02 17:53:55 +0300

    Update meta tags

--- src/Components/Random.js
+++ src/Components/Random.js
@@ -6,7 +6,7 @@ class Random extends Component{
   render(){
     return(
       <div className="Random">
-      <a className="btn btn-primary" onClick={this.props.onClick} > Need Random article? Try me</a>
+      <a className="btn btn-primary" onClick={this.props.onClick} > Need Random a article? Click me</a>
       </div>
     )
   }
